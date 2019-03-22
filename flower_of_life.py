8d312829d47694712ef96c9e9321259f7b5b78f7
Stephen <Stephen.duncanson@gmail.com>
2019-03-22 00:05:17 -0400

    Add files via upload

---
+++ flower_of_life.py
@@ -0,0 +1,95 @@
+#Flower of life
+#stephen duncanson
+
+import turtle
+turtle.hideturtle()
+turtle.speed(20) #set speed
+
+turtle.penup()
+turtle.goto(0,-120) #get to initial position
+
+turtle.pendown()
+turtle.circle(120) #draw big circle
+
+turtle.circle(40) #bottom middle
+turtle.penup()
+
+turtle.goto(0,-40)
+turtle.pendown()
+turtle.circle(40) # middle
+turtle.penup()
+
+turtle.goto(0,40)
+turtle.pendown()
+turtle.circle(40) #top middle
+
+turtle.penup()
+turtle.goto(69,0)
+turtle.pendown()
+turtle.circle(40) #top right
+turtle.penup()
+
+
+turtle.goto(69,-80)
+turtle.pendown()
+turtle.circle(40) #bottom right
+turtle.penup()
+turtle.goto(-69,-80) #bottom left
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(-69,0) #top left
+turtle.pendown()
+turtle.circle(40)
+
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(0,-80)
+turtle.pendown()
+turtle.circle(40)
+
+
+turtle.penup()
+turtle.goto(69,-40)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(-69,-40)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(35,20)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(-35,20)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(35,-100)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(-35,-100)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(-35,-20)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(35,-20)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(35,-60)
+turtle.pendown()
+turtle.circle(40)
+turtle.penup()
+turtle.goto(-35,-60)
+turtle.pendown()
+turtle.circle(40)
+
